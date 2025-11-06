5cba8fa8de95f26697cc17bb0a74296dcdc46361
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:23:15 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:23:10.012Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:23:14.736Z
+Commit: Fix memory leak in event handlers
