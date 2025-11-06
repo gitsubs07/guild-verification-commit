c11075713c12eb6529be180a348f7438755fc026
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:19:55 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:19:39.777Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:19:54.431Z
+Commit: Fix CSS styling bug on mobile devices
