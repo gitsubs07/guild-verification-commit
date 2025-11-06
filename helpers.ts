6ff46248d557864842832055e8ffae5908f69c52
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:29:11 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:28:48.586Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T01:29:10.816Z
+Commit: Fix memory leak in event handlers
