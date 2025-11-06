a469e2a3c3ae2ebf5930006c85cef36db5c411bf
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:20:40 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:20:02.676Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T01:20:39.155Z
+Commit: Resolve timezone issues in date handling
