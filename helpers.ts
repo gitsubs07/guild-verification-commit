dd84c42177daae883714d67fdc3bc79f071338a3
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:23:14 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:23:01.737Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T01:23:13.043Z
+Commit: Patch API endpoint error handling
