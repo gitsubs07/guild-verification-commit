6f6fd7df37c6fd86b19d19e4e6e2ac7e1a51def3
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:20:48 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:20:39.155Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T01:20:47.753Z
+Commit: Patch API endpoint error handling
