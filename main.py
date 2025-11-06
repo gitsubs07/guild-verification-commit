c415f0d901bc6213d8a3b3b5f47001432b85e3fc
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:16:55 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:16:47.785Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T01:16:54.113Z
+Commit: Add multi-language support
