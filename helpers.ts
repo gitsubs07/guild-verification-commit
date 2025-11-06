6ca00c6ab854d5ee2e0afde7090413357fa694a8
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:18:59 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:18:50.687Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T01:18:57.802Z
+Commit: Resolve timezone issues in date handling
