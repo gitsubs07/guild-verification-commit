ddefc9de49a459fa68f84753dbbec02776446556
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:30:16 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:30:11.148Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T01:30:15.439Z
+Commit: Correct typo in configuration file
