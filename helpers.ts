a55383c0f70b710c5e362805822570349f5d9e7d
gitsubs07 <123282908+gitsubs07@users.noreply.github.com>
2025-11-06 04:21:09 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T01:20:47.753Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T01:21:08.278Z
+Commit: Patch security vulnerability in authentication
